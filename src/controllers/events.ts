001051d1b35b5f45e28865518c6d856abd9a9779
Ali Salimli <67149699+elisalimli@users.noreply.github.com>
2024-03-15 12:27:44 +0400

    DM with Superbot (#12)

--- src/controllers/events.ts
+++ src/controllers/events.ts
@@ -6,21 +6,23 @@ const eventsService = new SlackEventsService();
 async function eventsController(req: Express.Request, res: Express.Response) {
   const body = req.body;
   const requestType = body?.type;
-  console.log("RECIEVED REQUEST EVENTS", req.body);
   if (requestType === "url_verification") {
     return res.send({
       challenge: body.challenge,
     });
   }
 
-  if (requestType === "event_callback") {
+  // listening for only non-bot messages
+  if (requestType === "event_callback" && !body?.event?.message?.bot_id) {
+    console.log("RECIEVED REQUEST EVENTS", body);
+
     const eventType = body?.event?.type;
 
-    const isFollowUp =
-      eventType === "message" && body?.event?.thread_ts && !body?.event?.bot_id;
+    const isFollowUp = eventType === "message" && body?.event?.thread_ts;
     const isAppMention = eventType === "app_mention";
+    const isDm = eventType === "message" && body?.event?.channel_type === "im";
 
-    if (isAppMention || isFollowUp)
+    if (isAppMention || isFollowUp || isDm)
       return await eventsService.answerQuestion(req, res);
   }
 
